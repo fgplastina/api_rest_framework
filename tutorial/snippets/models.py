7aea73c0267b02c3f2768a54ce6331b95d671b15
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2021-08-25 17:14:39 -0300

    Segundo tutorial de API REST

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -7,7 +7,7 @@ from pygments.styles import get_all_styles
 # Create your models here.
 
 LEXERS = [item for item in get_all_lexers() if item [1]]
-LANGUAGE_CHOICE = sorted([(item[1][0], item[0]) for item in LEXERS])
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item,item) for item in get_all_styles()])
 
 
@@ -17,7 +17,7 @@ class Snippet(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
-    language = models.CharField(choices=LANGUAGE_CHOICE, default='python', max_lengt=100)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly' , max_length=100)
 
     class Meta:
---
+++ tutorial/snippets/views_first_edition.py
@@ -0,0 +1,61 @@
+# from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+# Create your views here.
+
+
+#La vista marca como csrf_exempt porque para el ejemplo se requiere que un cliente sin el token pueda realizar un POST. En situaciones normales esto no es recomendable
+
+@csrf_exempt
+#cuando se usa @ antes de un def indica que es un decorator. Es decir, una funcion que toma otra funcion.
+def snippets_list(request):
+    """
+
+    Listar todos los codigos snippets o crear uno nuevo.
+
+    """
+
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method =='POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def snippets_detail(request, pk):
+    """
+
+    Retrieve, actualizar o eliminar un codigo snippet
+
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        #El snippet dado por parametro se obtiene del try 
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
