156b31186744bf55887064d4e7bfc0a66e38151e
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2021-08-25 13:44:49 -0300

    Commit inicial, creando app y modelos

---
+++ tutorial/snippets/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+#Un lexers permite dividir la fuente en tokens, fragmentos de la fuente que tienen un tipo de token determinando que texto reppresenta semanticamente. Al igual que en un IDE se diferencias las palabras clave como "class, method, def, etc".
+from pygments.lexers import get_all_lexers
+#Son estilos para "remarcar" 
+from pygments.styles import get_all_styles
+
+# Create your models here.
+
+LEXERS = [item for item in get_all_lexers() if item [1]]
+LANGUAGE_CHOICE = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item,item) for item in get_all_styles()])
+
+
+#Un snippet es una "porcion" o fraccion de codigo reutilizable. Tambien puede ser codigo maquina o texto.
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICE, default='python', max_lengt=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly' , max_length=100)
+
+    class Meta:
+        ordering = ['created']
+
