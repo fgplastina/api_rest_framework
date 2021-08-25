d7fcbd4a5149e0571450eef229c640aac9de9c00
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2021-08-25 17:47:05 -0300

    Finalizando tutorial 2 - Responses and Request

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.snippets_list),
-    path('snippets/<int:pk>', views.snippets_detail),
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
